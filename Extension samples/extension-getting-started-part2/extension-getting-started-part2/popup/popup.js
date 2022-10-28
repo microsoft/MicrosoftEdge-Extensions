477c3570e055bd4c46033bc1fff021b067d14685
JasonAndrewWriter <v-jandrew@microsoft.com>
2022-10-28 10:18:55 -0700

    Replacing code examples
    I worked with  Bhuvana Pappu to update this code example for this article: https://review.learn.microsoft.com/en-us/microsoft-edge/extensions-chromium/getting-started/part2-content-scripts?branch=pr-en-us-2266&tabs=v3

--- Extension samples/extension-getting-started-part2/extension-getting-started-part2/popup/popup.js
+++ Extension samples/extension-getting-started-part2/extension-getting-started-part2/popup/popup.js
@@ -1,25 +1,24 @@
 const sendMessageId = document.getElementById("sendmessageid");
 if (sendMessageId) {
-  sendMessageId.onclick = function() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-      chrome.tabs.sendMessage(
-        tabs[0].id,
-        {
-          url: chrome.extension.getURL("images/stars.jpeg"),
-          imageDivId: `${guidGenerator()}`,
-          tabId: tabs[0].id
-        },
-        function(response) {
-          console.log("message with url sent");
-          window.close();
-        }
-      );
-      function guidGenerator() {
-        const S4 = function () {
-          return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-        };
-        return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
-      }
-    });
-  };
+    sendMessageId.onclick = function() {
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+            chrome.tabs.sendMessage(
+                tabs[0].id,
+                {
+                    url: chrome.runtime.getURL("images/stars.jpeg"),
+                    imageDivId: `${guidGenerator()}`,
+                    tabId: tabs[0].id
+                },
+                function(response) {
+                    window.close();
+                }
+            );
+            function guidGenerator() {
+                const S4 = function () {
+                    return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+                };
+                return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+            }
+        });
+    };
 }
