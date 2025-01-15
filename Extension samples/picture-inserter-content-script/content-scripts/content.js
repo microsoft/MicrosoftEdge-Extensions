380851803e425a7a4b6d40760a482fd527241c7e
Michael S. Hoffman <v-mihoffman@microsoft.com>
2025-01-15 09:32:52 -0800

    remove jquery.min.js

--- Extension samples/picture-inserter-content-script/content-scripts/content.js
+++ Extension samples/picture-inserter-content-script/content-scripts/content.js
@@ -1,17 +1,13 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   const img = document.createElement("img");
-  img.src = request.url;
   img.id = request.imageDivId;
-  img.className = "slide-image";
-  document.querySelector("body").prepend(img);
+  img.src = request.url;
+  img.style = "height: auto; width: 97vw;";
+  document.body.prepend(img);
 
-  const style = document.createElement("style");
-  style.textContent = ".slide-image {height: auto; width: 97vw;}";
-  document.querySelector("head").prepend(style);
-
-  $(`#${request.imageDivId}`).click(function() {
-      $(`#${request.imageDivId}`).remove(`#${request.imageDivId}`);
-  });
+  img.addEventListener("click", () => {
+      img.remove();
+  }, { once: true });
 
   sendResponse({ fromcontent: "This message is from content.js" });
 });
